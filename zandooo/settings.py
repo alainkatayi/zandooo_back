f1bfed275d2aff2ba0338e4dcf57b65f809c7efa
alainkatayi <alainkatayi01@gmail.com>
2025-10-31 15:12:54 +0100

    Ajout du corsheaders dans le MIDDLEWARE de l'application

--- zandooo/settings.py
+++ zandooo/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,6 +129,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+CORS_ALLOW_ALL_ORIGINS = True 
 
 
 # Static files (CSS, JavaScript, Images)
