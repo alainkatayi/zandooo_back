c91a027028c886390d667aa762c2e666f7175a11
alainkatayi <alainkatayi01@gmail.com>
2025-11-25 15:45:25 +0100

    Implémentation de la class ShopUpdatedView, qui nous permet de mettre à jour une boutique

--- shops/urls.py
+++ shops/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ShopCreatedView
+from .views import ShopCreatedView, ShopUpdatedView
 
 urlpatterns = [
     path('store/', ShopCreatedView.as_view(), name='create_store'),
+    path('<int:pk>/update/', ShopUpdatedView.as_view(), name='update_store'),
 ]
--- shops/views.py
+++ shops/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from .serializers import ShopSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Shop
 
 # Create your views here.
 class ShopCreatedView(APIView):
@@ -18,3 +19,24 @@ class ShopCreatedView(APIView):
                 "Shop": ShopSerializer(shop).data},
                 status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ShopUpdatedView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def put(self,request,pk):
+        shop = get_object_or_404(Shop, pk=pk)
+        if shop.owner != request.user:
+            return Response({
+                "Message": "You do not have the right to modify this shop information"
+            }, status= status.HTTP_401_UNAUTHORIZED)
+        else:
+            serializer = ShopSerializer(shop, data= request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    "Message":"Shop modified successfully"
+                },status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    "Error": serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST)
