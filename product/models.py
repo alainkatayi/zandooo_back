944a2a39c42407d24410052d25a529403a53d5da
alainkatayi <alainkatayi01@gmail.com>
2026-01-21 16:38:09 +0100

    Création du serializer de product

--- product/models.py
+++ product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from abstracts_models.soft_delete_model import SoftDeleteModel
 from manager.soft_delete_manager import SoftDeleteManager
-from accounts.models import User
+from shops.models import Shop
 from abstracts_models.time_stamped_model import TimeStampedModel
 
 # Create your models here.
@@ -13,4 +13,4 @@ class Product(TimeStampedModel,SoftDeleteModel):
     price = models.FloatField()
     image = models.ImageField(upload_to='products_images/', null=False)
     stock = models.IntegerField()
-    shop = models.ForeignKey(User,on_delete=models.CASCADE,related_name='store')
+    shop = models.ForeignKey(Shop,on_delete=models.CASCADE,related_name='store')
