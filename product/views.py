7c781d13e08b162577b2d7515d4b7f3f4a022b53
alainkatayi <alainkatayi01@gmail.com>
2026-04-16 14:18:38 +0100

    feat(product):Implémentation de la logique ProductOneView, qui permet de recuperer un seul produit

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .serializers import ProductSerializer
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
@@ -92,4 +92,13 @@ class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductPagination
+
+#class pour recuperer un produit en particulier
+class ProductOneView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request,pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return  Response(serializer.data)
         
